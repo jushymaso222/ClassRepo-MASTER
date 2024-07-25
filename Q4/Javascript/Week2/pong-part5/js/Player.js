500ef19f35761200afefd4228c148e42b0c54a94
jushymaso222 <92840402+jushymaso222@users.noreply.github.com>
2024-07-25 12:49:46 -0400

    week2 part5

---
+++ Q4/Javascript/Week2/pong-part5/js/Player.js
@@ -0,0 +1,8 @@
+class Player {
+    constructor(name, score, highScore, pad) {
+        this.name = name;
+        this.score = score;
+        this.highScore = highScore;
+        this.pad = pad;
+    }
+}
